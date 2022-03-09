606a67fc528b200f830094d056061c5aa56bc47d
Elden <mceldenn@gmail.com>
2022-03-09 18:22:42 +0100

    1.4.0 (#1)
    * Create .gitignore
    
    * Updated CivFR Squadron preset mappool
    
    * Added Default Tournament Name in preset
    
    * Update modinfo to 1.4.0
    
    * Added Official tournament game button
    
    * updated banned wonders for squadron preset

--- ui/hostgame.lua
+++ ui/hostgame.lua
@@ -840,8 +840,31 @@ end
 function Squadron_Natural_Wonders()
 	local default = {}
 	default = {
+                "FEATURE_BARRINGER_CRATER",
+				"FEATURE_BIOLUMINESCENT_BAY",
+				"FEATURE_CERRO_DE_POTOSI",
+				"FEATURE_DALLOL",
+				"FEATURE_GRAND_MESA",
+				"FEATURE_KRAKATOA",
+				"FEATURE_LAKE_VICTORIA",
+				"FEATURE_LENCOIS_MARANHENSES",
+				"FEATURE_OUNIANGA",
+				"FEATURE_MOSI_OA_TUNYA",
+				"FEATURE_MOTLATSE_CANYON",
+				"FEATURE_KAILASH",
+				"FEATURE_NAMIB",
+				"FEATURE_OLD_FAITHFUL",
+				"FEATURE_SINAI",
+				"FEATURE_SALAR_DE_UYUNI",
+				"FEATURE_WULINGYUAN",
+				"FEATURE_SALAR_DE_UYUNI",
+				"FEATURE_SRI_PADA",
+				"FEATURE_GIBRALTAR",
+				"FEATURE_VREDEFORT_DOME",
 				"FEATURE_LYSEFJORDEN",
-				"FEATURE_GIANTS_CAUSEWAY"
+				"FEATURE_GIANTS_CAUSEWAY",
+				"FEATURE_BERMUDA_TRIANGLE",
+				"FEATURE_MATTERHORN"
 				}
 	GameConfiguration.SetValue("EXCLUDE_NATURAL_WONDERS",default)
 end
@@ -875,6 +898,7 @@ function CheckPreset()
 		-- Squadron
 		if currentPreset == 3 then
 			print("Applied Squadron Settings")
+			Default_Natural_Wonders()
 			Squadron_Natural_Wonders()
 		end	
 		Network.BroadcastGameConfig();	
